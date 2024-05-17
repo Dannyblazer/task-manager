94a7f1b407057c5c9242cb8b3e296d7ac41eb453
Dannyblazer <nnajidanny004@gmail.com>
2024-05-17 13:00:57 +0100

    injected the UsePipe and ValidationPipe for DTOs

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+
+import { Controller, Get, Post, Body, Param, Delete, Patch, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
-export class TasksController {}
+@UseGuards(AuthGuard('jwt'))
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Get(':id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete(':id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+
+  @Patch(':id/status')
+  @UsePipes(ValidationPipe)
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, updateTaskStatusDto);
+  }
+}
