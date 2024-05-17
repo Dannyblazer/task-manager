5e4a662d5cb88e1a0e1383e90ea6f41604886b87
Dannyblazer <nnajidanny004@gmail.com>
2024-05-17 12:36:50 +0100

    injected events Gateway and module for sockets update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { EventsGateway } from './events/events.gateway';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [TasksModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TasksModule, AuthModule, UsersModule, EventsModule],
+  providers: [EventsGateway],
 })
 export class AppModule {}
