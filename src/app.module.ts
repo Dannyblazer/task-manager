4415ca83d11a45ed7554fdd09653848a7229d06c
Dannyblazer <nnajidanny004@gmail.com>
2024-05-17 13:24:02 +0100

    database connection setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -7,7 +8,17 @@ import { EventsGateway } from './events/events.gateway';
 import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [TasksModule, AuthModule, UsersModule, EventsModule],
+  imports: [TasksModule, AuthModule, UsersModule, EventsModule, TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    password: 'pass123',
+    username: 'taskit',
+    entities: [],
+    database: 'task_db',
+    synchronize: true,
+    logging: true,
+  })],
   providers: [EventsGateway],
 })
 export class AppModule {}
