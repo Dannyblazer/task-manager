6db0f863c97e9cc49d10c1aff26f78f9a2e66396
Nnaji Daniel <43733933+Dannyblazer@users.noreply.github.com>
2024-05-18 14:31:49 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,41 @@
-
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { EventsGateway } from './events/events.gateway';
 import { EventsModule } from './events/events.module';
+import { Task } from './tasks/tasks.entity';
+import { User } from './users/user.entity';
+import { DatabaseTestService } from './database-test.service';
 
 @Module({
-  imports: [TasksModule, AuthModule, UsersModule, EventsModule, TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    password: secretpass, // remember to set environ
-    username: environusername, // This too
-    entities: [],
-    database: 'task_db',
-    synchronize: true,
-    logging: true,
-  })],
-  providers: [EventsGateway],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [Task, User],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+    TypeOrmModule.forFeature([Task]),
+    TasksModule,
+    AuthModule,
+    UsersModule,
+    EventsModule,
+  ],
+  providers: [DatabaseTestService, EventsGateway],
 })
 export class AppModule {}
+
