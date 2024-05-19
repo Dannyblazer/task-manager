7848ad55e3a5e7144d5cb93b12122f102a0e2902
Dannyblazer <nnajidanny004@gmail.com>
2024-05-19 23:35:20 +0100

    Added comments and swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
-
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+
+  // Configure Swagger options
+  const config = new DocumentBuilder()
+    .setTitle('Task Manager API')
+    .setDescription('API documentation for the Task Manager application')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
