dc309f2cc8d51fd170f15d06aa924dce6c975b07
Dannyblazer <nnajidanny004@gmail.com>
2024-05-19 23:46:56 +0100

    socket stream module fix

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
+import { AuthModule } from '../auth/auth.module'; // Import AuthModule to enable authentication
+import { ConfigModule } from '@nestjs/config';
 
+// Module decorator to define the EventsModule
 @Module({
-  providers: [EventsGateway],
-  exports: [EventsGateway],
+  imports: [
+    AuthModule, // Ensure AuthModule is imported here to enable authentication
+    ConfigModule,
+  ],
+  providers: [EventsGateway], // Providers array to specify the providers that should be available in the EventsModule
 })
-export class EventsModule {}
+export class EventsModule {} // Export the EventsModule class
