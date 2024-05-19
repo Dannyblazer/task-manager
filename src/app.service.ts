6581978977c9480eb48dad9961c8c2b7f5b668c2
Dannyblazer <nnajidanny004@gmail.com>
2024-05-19 23:34:14 +0100

    comments on app services

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,9 @@
+// Import necessary decorator from NestJS
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable() // Marks this class as a provider that can be injected into other components
 export class AppService {
+  // Method to return a greeting message
   getHello(): string {
     return 'Hello World!';
   }
